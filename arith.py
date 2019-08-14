f95c4eb34530337f1d2bea47357ada0290d23024
MEERAMURUGAIYAN <52280782+MEERAMURUGAIYAN@users.noreply.github.com>
2019-08-14 14:22:26 +0530

    Create arith.py

---
+++ arith.py
@@ -0,0 +1,7 @@
+    
+#m
+x,y=list(map(int,input().split()))
+i,j=list(map(int,input().split()))
+k,l=list(map(int,input().split()))
+if(x==i==k) or (y==j==l) or (x==y and i==j and k==l):
+    print("yes")
